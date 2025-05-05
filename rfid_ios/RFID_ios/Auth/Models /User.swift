cb91391736662722e4923849280e959ce65ab766
iwata1107 <iwata.s1107@gmail.com>
2025-05-05 23:21:38 +0900

    update:

---
+++ rfid_ios/RFID_ios/Auth/Models /User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  RFID_ios
+//
+//  Created by 岩田照太 on 2025/05/05.
+//
+
+import Foundation
---
+++ rfid_ios/RFID_ios/Auth/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  RFID_ios
+//
+//  Created by 岩田照太 on 2025/05/05.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
